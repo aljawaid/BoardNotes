1e25cd5e09b0760734adbc6831506886d04e10ac
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-07-15 08:18:16 +0100

    Fix Mysql.php
    #2

--- Schema/Mysql.php
+++ Schema/Mysql.php
@@ -11,41 +11,29 @@ const VERSION = 1;
 
 function version_1(PDO $pdo)
 {
-
-    $pdo->exec('INSERT INTO boardnotes_cus (
-        project_id,
-        project_name)
-        VALUES (
-            9998,
-            "General"
-        )
-    ');
-    $pdo->exec('INSERT INTO boardnotes_cus (
-        project_id,
-        project_name)
-        VALUES (
-            9997,
-            "Todo"
-        )
-    ');
-
     $pdo->exec('CREATE TABLE IF NOT EXISTS boardnotes_cus (
-        "id" INTEGER PRIMARY KEY,
-        "project_id" INTEGER NOT NULL,
-        "project_name" TEXT
-    )');
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `project_id` INTEGER NOT NULL,
+        `project_name` TEXT,
+        PRIMARY KEY(id)
+    ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci');
 
 
     $pdo->exec('CREATE TABLE IF NOT EXISTS boardnotes (
-        "id" INTEGER PRIMARY KEY,
-        "project_id" INTEGER NOT NULL,
-        "user_id" INTEGER NOT NULL,
-        "position" INTEGER,
-        "is_active" INTEGER,
-        "title" TEXT,
-        "category" TEXT,
-        "description" TEXT,
-        "date_created" INTEGER,
-        "date_modified" INTEGER
-    )');
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `project_id` INT NOT NULL,
+        `user_id` INT NOT NULL,
+        `position` INT,
+        `is_active` INT,
+        `title` TEXT,
+        `category` TEXT,
+        `description` TEXT,
+        `date_created` INT,
+        `date_modified` INT,
+        PRIMARY KEY(id)
+    ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci');
+
+    $pdo->exec('INSERT INTO `boardnotes_cus`(`project_id`, `project_name`) VALUES (9998, "General")');
+
+    $pdo->exec('INSERT INTO `boardnotes_cus`(`project_id`, `project_name`) VALUES (9997, "Todo")');
 }
